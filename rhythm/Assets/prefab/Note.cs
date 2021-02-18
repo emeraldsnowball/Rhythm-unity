8b3b47b4b29cb0a8d743904f3be27ad26e7ee6a9
mable <68956989+emeraldsnowball@users.noreply.github.com>
2021-02-18 11:58:44 +0000

    added player activator and note

---
+++ rhythm/Assets/prefab/Note.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float moveSpeed = 3f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.localPosition -= Vector3.up * moveSpeed * Time.deltaTime;
+
+        //transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y * moveSpeed * Time.deltaTime, transform.localPosition.z);
+
+        if (transform.localPosition.y <= -10f)
+        {
+            if(gameObject != null)
+                Destroy(gameObject);
+        }
+    }
+}
