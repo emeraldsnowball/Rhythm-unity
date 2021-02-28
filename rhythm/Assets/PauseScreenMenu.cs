5ca5caf2bfad0fa153291c1d8269bf3576b2c955
mable <68956989+emeraldsnowball@users.noreply.github.com>
2021-02-28 09:15:18 +0000

    Startup screen

--- rhythm/Assets/PauseScreenMenu.cs
+++ rhythm/Assets/PauseScreenMenu.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PauseScreenMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
 
     public static bool GameIsPaused = false;
 
---
+++ rhythm/Assets/StartupScreenMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartupScreenMenu : MonoBehaviour
+{
+    public GameObject startupMenuUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startupMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        //GameIsPaused = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            startupMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            //GameIsPaused = false;
+        }
+    }
+}
